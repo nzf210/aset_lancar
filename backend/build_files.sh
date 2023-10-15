bba158b84a2e15c925c02493261d70aab9966358
Syahril S <syahrilsymd@gmail.com>
2023-10-16 00:37:36 +0800

    change build_files

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -2,5 +2,4 @@ pip install -r requirements.txt
 # python3.9 manage.py makemigrations
 # python3.9 manage.py migrate
 python3.9 main/manage.py collectstatic --noinput
-python3.9 main/manage.py findstatic
 # python3.9 manage.py runserver 0.0.0.0:8000
