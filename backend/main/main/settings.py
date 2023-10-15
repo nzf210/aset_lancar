537b6e5ae83d2e764318b0317def14353ba7e1ae
Syahril S <syahrilsymd@gmail.com>
2023-10-16 02:14:35 +0800

    try remove db

--- backend/main/main/settings.py
+++ backend/main/main/settings.py
@@ -75,22 +75,20 @@ WSGI_APPLICATION = "main.wsgi.app"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default_db": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     },
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": os.environ.get("POSTGRES_DB_NAME"),
-#         "USER": os.environ.get("POSTGRES_USER"),
-#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-#         "HOST": os.environ.get("POSTGRES_HOST"),
-#         "PORT": os.environ.get("POSTGRES_PORT"),
-#     },
-# }
-
-DATABASES = {}
+DATABASES = {
+    "default_db": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("POSTGRES_DB_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -137,6 +135,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-# APPEND_SLASH = False
 
 AUTH_USER_MODEL = "auth_user.User"
--- backend/main/main/wsgi.py
+++ backend/main/main/wsgi.py
@@ -5,12 +5,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
-
-app = get_wsgi_application()
+os.environ["DJANGO_SETTINGS_MODULE"] = "main.settings"
+aplication = get_wsgi_application()
+app = aplication
