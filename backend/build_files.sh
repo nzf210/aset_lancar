1ae3caf7a6753a024ea26d33c946e320e3054620
Syahril S <syahrilsymd@gmail.com>
2023-10-16 20:02:52 +0800

    tx1

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -4,4 +4,4 @@ mkdir -p staticfiles_build/static
 pip install -r requirements.txt
 # python3.9 manage.py makemigrations 
 python3.9 manage.py migrate 
-python3.9 manage.py collectstatic --noinput
+#python3.9 manage.py collectstatic --noinput
