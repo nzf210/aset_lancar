bf86737ae86474d564ce0f0b5947896d1075931c
Syahril S <syahrilsymd@gmail.com>
2023-10-16 18:55:58 +0800

    instal req

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,7 +1,7 @@
 mkdir -p static
 mkdir -p staticfiles_build/static
 # mkdir -p main/static
-# pip install -r requirements.txt
+pip install -r requirements.txt
 # python3.9 manage.py makemigrations 
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic --noinput
